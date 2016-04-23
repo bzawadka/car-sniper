717c1f4f1a34c10cd9ca26ef263d8b9e199bb2fa
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2016-04-23 22:32:14 +0200

    initial app

---
+++ src/main/java/pl/bzawadka/CarController.java
@@ -0,0 +1,5 @@
+package pl.bzawadka;
+
+public class CarController {
+
+}
---
+++ src/main/java/pl/bzawadka/CarSniperApplication.java
@@ -0,0 +1,12 @@
+package pl.bzawadka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSniperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSniperApplication.class, args);
+	}
+}
