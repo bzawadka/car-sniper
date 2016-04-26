d92ab14f380c023d9815d98e9d745cf1a625de18
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2016-04-26 08:17:15 +0200

    parsing html to a Car - url

--- src/main/java/pl/bzawadka/controller/CarController.java
+++ src/main/java/pl/bzawadka/controller/CarController.java
@@ -6,6 +6,9 @@ import pl.bzawadka.model.Car;
 import pl.bzawadka.model.Engine;
 import pl.bzawadka.model.Price;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import static pl.bzawadka.model.Currency.CHF;
 import static pl.bzawadka.model.Fuel.DIESEL;
 import static pl.bzawadka.model.Make.AUDI;
@@ -15,7 +18,7 @@ import static pl.bzawadka.model.SaleType.AUCTION;
 public class CarController {
 
     @RequestMapping(path = "/car")
-    public Car getCar() {
+    public Car getCar() throws MalformedURLException {
         return Car.builder()
                 .setMake(AUDI)
                 .setModel("A4")
@@ -30,6 +33,7 @@ public class CarController {
                         .setSize(2.0)
                         .setPower(140)
                         .createEngine())
+                .setUrl(new URL("http://car-sniper.ch/1234"))
                 .createCar();
     }
 }
