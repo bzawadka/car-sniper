6f524f4633e7dbdb55d08b2c8430b8aa38080dc3
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2016-05-05 14:24:35 +0200

    more cars

--- src/main/java/pl/bzawadka/Application.java
+++ src/main/java/pl/bzawadka/Application.java
@@ -13,9 +13,9 @@ public class Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... strings) throws Exception {
-		this.carRepository.deleteAll();
-		carRepository.index(Garage.audiA4());
-		carRepository.index(Garage.audiA5());
+		//this.carRepository.deleteAll();
+		//carRepository.index(Garage.mazda3());
+		//carRepository.index(Garage.mazda6());
 	}
 
 	public static void main(String[] args) {
--- src/main/java/pl/bzawadka/Garage.java
+++ src/main/java/pl/bzawadka/Garage.java
@@ -9,7 +9,9 @@ import java.net.URL;
 
 import static pl.bzawadka.model.Currency.CHF;
 import static pl.bzawadka.model.Fuel.DIESEL;
+import static pl.bzawadka.model.Fuel.PETROL;
 import static pl.bzawadka.model.Make.AUDI;
+import static pl.bzawadka.model.Make.MAZDA;
 import static pl.bzawadka.model.SaleType.AUCTION;
 
 public class Garage {
@@ -19,7 +21,30 @@ public class Garage {
     }
 
     public static Car audiA5() {
-        return audiA4builder().withModel("A5").withYear(2015).build();
+        return audiA4builder().withModel("A5").withPrice(chfPrice(34000)).withYear(2015).build();
+    }
+
+    public static Car mazda3() {
+        return mazda3Builder().build();
+    }
+
+    public static Car mazda6() {
+        return mazda3Builder().withModel("6").withPrice(chfPrice(23000)).withYear(2015).build();
+    }
+
+    private static Car.Builder mazda3Builder() {
+        return Car.builder()
+                .withMake(MAZDA)
+                .withModel("3")
+                .withYear(2014)
+                .withPrice(chfPrice(15400))
+                .withSaleType(AUCTION)
+                .withEngine(Engine.builder()
+                        .withFuel(PETROL)
+                        .withSize(2.0)
+                        .withPower(165)
+                        .build())
+                .withUrl(url());
     }
 
     private static Car.Builder audiA4builder() {
@@ -27,10 +52,7 @@ public class Garage {
                 .withMake(AUDI)
                 .withModel("A4")
                 .withYear(2008)
-                .withPrice(Price.builder()
-                        .withPrice(10000)
-                        .withCurrency(CHF)
-                        .build())
+                .withPrice(chfPrice(10000))
                 .withSaleType(AUCTION)
                 .withEngine(Engine.builder()
                         .withFuel(DIESEL)
@@ -38,6 +60,13 @@ public class Garage {
                         .withPower(140)
                         .build())
                 .withUrl(url());
+    }
+
+    private static Price chfPrice(int price) {
+        return Price.builder()
+                .withPrice(price)
+                .withCurrency(CHF)
+                .build();
     }
 
     private static URL url() {
